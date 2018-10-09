0a0031b276b02cc761339dc1659c90e050141c14
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-09 14:15:14 -0600

    passing US 1 locally, new PORO class with model spec

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def show
+    id = params[:id]
+    #set env variable for endpoint
+    # for now hardcode the heroku link
+    @conn = Faraday.new(url: "http://localhost:3000")
+
+    response = @conn.get("/api/v1/users/#{id}")
+    data = JSON.parse(response.body, symbolize_names: true)
+    @user = AppUser.new(data)
+  end
+end
