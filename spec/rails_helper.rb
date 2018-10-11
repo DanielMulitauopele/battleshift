1a3fca8c721f11d341cd8a10f63e2f0aabdc2b7f
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-11 15:13:55 -0600

    adds gems webmock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,13 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+end
 
 SimpleCov.start "rails"
 
