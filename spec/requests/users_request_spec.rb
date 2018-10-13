80bf9d7ccae07dab4be9c2b6fb0011f3a54118dc
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-13 13:57:34 -0600

    adds password to user factory spec, adds passwords to spec test setup

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -23,8 +23,8 @@ describe "users api endpoints" do
 
   context "GET /api/v1/users/:id" do
     it "returns a specific user" do
-      cj   = User.create!(name: "C.J. Cregg", id: 111)
-      toby = User.create!(name: "Toby Ziegler", id: 222)
+      cj   = User.create!(name: "C.J. Cregg", id: 111, email: "hi@hi.com", password: "x")
+      toby = User.create!(name: "Toby Ziegler", id: 222, email: "hi@hi.com", password: "x")
 
         get "/api/v1/users/#{cj.id}"
 
@@ -41,7 +41,7 @@ describe "users api endpoints" do
 
   context "PATCH /api/v1/users/:id" do
     it 'edits a specific user' do
-      cj   = User.create!(name: "C.J. Cregg", id: 111, email: "cj@example.com")
+      cj   = User.create!(name: "C.J. Cregg", id: 111, email: "cj@example.com", password: "x")
       new_email = "cj@cj.com"
 
         patch "/api/v1/users/#{cj.id}", params: {email: new_email}
