5ec4e9e4fd02bdf4f73e14875bbbd49307a92119
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-15 13:30:55 -0600

    refactor sessions controller with sessions helper module method log_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password]) && user.activated == true
-      session[:user_id] = user.id
+      log_in(user)
       flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user)
     elsif
       user && user.authenticate(params[:password]) && user.activated == false
-      session[:user_id] = user.id
+      log_in(user)
       flash[:notice] = "This account has not yet been activated. Please check your email."
       redirect_to root_url
     else
