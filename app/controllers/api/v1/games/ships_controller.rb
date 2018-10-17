1ad3016317c51a78e0d8680b7e7465faf8b8ffc4
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-16 19:21:51 -0600

    progress on game spec

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -5,10 +5,31 @@ module Api
         def create
           game = Game.find(params[:game_id])
           ship = Ship.new(params[:ship_size])
-          # Method that "places" ship which adds message to game
-          # if ShipPlacer.new is valid
-          game.messages = "hi"
-          render json: game
+          p1_placer = ShipPlacer.new(board: game.player_1_board,
+                                  ship: ship,
+                                  start_space: params[:start_space],
+                                  end_space: params[:end_space]
+                                )
+          p1_placer.run
+          p2_placer = ShipPlacer.new(board: game.player_2_board,
+                                  ship: ship,
+                                  start_space: params[:start_space],
+                                  end_space: params[:end_space]
+                                )
+          p2_placer.run
+
+          game.save
+
+          render json: game, message: success_message(params[:ship_size])
+        end
+
+        private
+        def success_message(ship_size)
+          if ship_size == 3
+            "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
+          else
+            "Successfully placed ship with a size of 2. You have 0 ship(s) to place."
+          end
         end
       end
     end
--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -23,23 +23,59 @@ class TurnProcessor
 
   attr_reader :game, :target
 
-  def attack_opponent
-    result = Shooter.fire!(board: opponent.board, target: target)
-    @messages << "Your shot resulted in a #{result}."
-    game.player_1_turns += 1
-  end
-
-  def ai_attack_back
-    result = AiSpaceSelector.new(player.board).fire!
-    @messages << "The computer's shot resulted in a #{result}."
-    game.player_2_turns += 1
-  end
-
   def player
     Player.new(game.player_1_board)
   end
 
   def opponent
     Player.new(game.player_2_board)
+  end
+
+  def attack_opponent
+    if game.current_turn == "challenger"
+      result = Shooter.fire!(board: opponent.board, target: target)
+      @messages << "Your shot resulted in a #{result}."
+      if game.player_1_turns == nil
+        game.player_1_turns = 1
+      else
+        game.player_1_turns += 1
+      end
+    else
+      @messages << "Invalid move. It's your opponent's turn"
+    end
+    change_turn
+  end
+
+  def ai_attack_back
+    if game.current_turn == "computer"
+      if @target == nil
+        result = AiSpaceSelector.new(player.board).fire!
+        @messages << "The computer's shot resulted in a #{result}."
+        if game.player_2_turns == nil
+          game.player_2_turns = 1
+        else
+          game.player_2_turns += 1
+        end
+      else
+        AiSpaceSelector.new(player.board, @target).fire!
+        @messages << "The computer's shot resulted in a #{result}."
+        if game.player_2_turns == nil
+          game.player_2_turns = 1
+        else
+          game.player_2_turns += 1
+        end
+      end
+    else
+      @messages << "Invalid move. It's your opponent's turn"
+    end
+    change_turn
+  end
+
+  def change_turn
+    if game.current_turn == "challenger"
+      game.current_turn = "computer"
+    else
+      game.current_turn = "challenger"
+    end
   end
 end
