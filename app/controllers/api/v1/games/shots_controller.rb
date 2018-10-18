f86ce8f8be8957480e83f077e3e4e49b53ce57b9
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-18 15:52:11 -0600

    adds api key identification for the shots controller, can fire shots and will determine who's turn it is. Checks for valid shots

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -4,11 +4,30 @@ module Api
       class ShotsController < ApiController
         def create
           game = Game.find(params[:game_id])
-          user = User.find_by(api_key: request.headers["X-API-Key"])
-          turn_processor = TurnProcessor.new(game, params[:shot][:target])
-
-          turn_processor.run!
-          render json: game, message: turn_processor.message
+          if game.winner == nil
+            if game.valid_space?(params[:target])
+              if request.headers["X-API-Key"] == game.player_1_api_key && game.current_turn == "player_1"
+                turn_processor = TurnProcessor.new(game, params[:shot][:target], game.player_2_board)
+                turn_processor.run!
+                render json: game, message: turn_processor.message
+              elsif request.headers["X-API-Key"] == game.player_2_api_key && game.current_turn == "player_2"
+                turn_processor = TurnProcessor.new(game, params[:shot][:target], game.player_1_board)
+                turn_processor.run!
+                render json: game, message: turn_processor.message
+              elsif request.headers["X-API-Key"] == game.player_2_api_key && game.current_turn == "player_1"
+                render json: game, status: 400, message: "Invalid move. It's your opponent's turn"
+              elsif request.headers["X-API-Key"] == game.player_1_api_key && game.current_turn == "player_2"
+                render json: game, status: 400, message: "Invalid move. It's your opponent's turn"
+              else
+              end
+            else
+              render json: game, status: 400, message: "Invalid coordinates"
+            end
+          else
+            # End the game
+            # Render "Player is the winner!"
+            render message: "Winner winner chicken dinner"
+          end
         end
       end
     end
