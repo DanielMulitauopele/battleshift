02ae53a89ff611e6a2eaac5b965bdfb605fdf18c
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-20 10:38:41 -0600

    adds spec attributes test

--- spec/models/app_user_spec.rb
+++ spec/models/app_user_spec.rb
@@ -7,4 +7,13 @@ describe AppUser, type: :model do
 
     expect(app_user).to be_a(AppUser)
   end
+
+  it 'has attributes' do
+    attributes = {id: 1, name: "Rainbow", email: "rainbow@rainbow.com"}
+    app_user = AppUser.new(attributes)
+
+    expect(app_user.id).to eq(attributes[:id])
+    expect(app_user.name).to eq(attributes[:name])
+    expect(app_user.email).to eq(attributes[:email])
+  end
 end
